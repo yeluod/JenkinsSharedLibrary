a403602b1d65952b2249f8942695c6028fe1b9be
yeluo <yeluo0528@gamil.com>
2022-11-25 18:52:41 +0800

    测试libraryResource

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -1,3 +1,5 @@
+import com.deploy.helper.NpmHelper
+import com.deploy.helper.YarnHelper
 import com.deploy.tools.Tools
 import com.deploy.helper.GitHelper
 import com.deploy.helper.JavaHelper
@@ -10,8 +12,8 @@ def call() {
     def tools = Tools.read(this)
     def gitHelper = new GitHelper(this, tools)
     def javaHelper =  new JavaHelper(this, tools)
-    def npmHelper =  new JavaHelper(this, tools)
-    def yarnHelper =  new JavaHelper(this, tools)
+    def npmHelper =  new NpmHelper(this, tools)
+    def yarnHelper =  new YarnHelper(this, tools)
     /************************************************/
 
     pipeline {
