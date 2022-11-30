4c82fb4ad49119d3358949f30e5f8cac119afe00
yeluo <yeluo0528@gamil.com>
2022-11-30 11:29:25 +0800

    完善 NodeHelper

--- src/com/deploy/helper/NodeHelper.groovy
+++ src/com/deploy/helper/NodeHelper.groovy
@@ -1,6 +1,6 @@
 package com.deploy.helper
 
-
+import cn.hutool.core.util.StrUtil
 import com.deploy.helper.param.NodeToolParam
 
 /**
@@ -14,18 +14,21 @@ abstract class NodeHelper extends BaseHelper {
     protected def script
     protected NodeToolParam param
 
+    static final def INSTALL = 'install'
+    static final def BUILD = 'install'
+
+    static final def registry = '--registry'
+
     NodeHelper(script) {
         this.script = script
     }
 
     NodeHelper(script, String tool) {
-
         this.script = script
         this.param.tool = tool
     }
 
     NodeHelper(script, String tool, String registry) {
-
         this.script = script
         this.param.tool = tool
         this.param.registry = registry
@@ -39,5 +42,31 @@ abstract class NodeHelper extends BaseHelper {
         this.script.sh "${this.param.tool} -v"
     }
 
+    void install() {
+        this.install(null)
+    }
 
+    void install(def express) {
+        this.script.sh"""
+            ${this.param.tool} ${INSTALL} ${express}
+        """
+    }
+
+    void build() {
+        this.build(null)
+    }
+
+    void build(def express) {
+        this.script.sh"""
+            ${this.param.tool} ${this.getNodeCommand()} ${BUILD} ${express}
+        """
+    }
+
+    def getNodeCommand() {
+        if (StrUtil.isNotBlank(this.param.registry)) {
+            return "${registry} ${this.param.registry}"
+        } else {
+            return ''
+        }
+    }
 }
